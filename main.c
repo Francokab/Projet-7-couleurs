cccc41438509f3cc126204679e8a83fb53b0ecdf
Francokab <francoiskabiry@yahoo.fr>
2021-03-17 16:53:14 +0000

    added majCoup and affichage

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#define taillem 30
+#define taillem 5
 #define nb_coul 7
 
 char couleur[nb_coul]={'A','B','C','D','E','F','G'};
@@ -17,14 +17,26 @@ void affichage(){
   }
 }
 
-void majCoup(char couleur, char joueur){ // si la couleur selectionné pa
+void majCoup(char couleur, char joueur){ // si la couleur selectionné par le joueur est adjacente à une case joueur, on la change en case joueur 
   int test = 1;
   while (test == 1){
     test = 0;
-    for (int i = 0; i < taillem; i++){
+    for (int i = 0; i < taillem; i++){ // on parcourt le monde
 		  for (int j = 0; j < taillem; j++){
-        if (monde[i][j]==couleur){
-          if(monde[i-1][j]==joueur || monde[i][j+1]==joueur || monde[i+1][j]==joueur || monde[i][j-1]==joueur){
+        if (monde[i][j]==couleur){ // si la case est de la couleur sélectionné
+          if(monde[i-1][j]==joueur && i!=0){
+            monde[i][j]=joueur;
+            test = 1;
+          }
+          else if(monde[i+1][j]==joueur && i!=taillem-1){
+            monde[i][j]=joueur;
+            test = 1;
+          }
+          else if(monde[i][j-1]==joueur && j!=0){
+            monde[i][j]=joueur;
+            test = 1;
+          }
+          else if(monde[i][j+1]==joueur && j!=taillem-1){
             monde[i][j]=joueur;
             test = 1;
           }
@@ -45,12 +57,13 @@ int main(){
         }
       }
   }
-  affichage();
+  affichage(); // on affiche ml emonde 
   char symbole_joueur;
   char couleur_tour;
-  scanf("%d", symbole_joueur);
-  scanf("%d", couleur_tour)
-  majCoup(couleur_tour, char joueur)
+  printf("indiquer le joueur et la couleur de la forme joueur couleur\n");
+  scanf("%c %c", &symbole_joueur, &couleur_tour);
+  majCoup(couleur_tour, symbole_joueur);
+  affichage();
   return 0;
 }
 
